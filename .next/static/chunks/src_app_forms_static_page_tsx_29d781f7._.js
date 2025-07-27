946c598b8e22fd654888571ab3feeba8ef29c94d
Monica R <138276052+monica-r56@users.noreply.github.com>
2025-07-27 13:07:26 +0530

    Add .next/static/chunks/src_app_forms_static_page_tsx_29d781f7._.js 1753601845960

---
+++ .next/static/chunks/src_app_forms_static_page_tsx_29d781f7._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_d4026c29._.js",
+  "static/chunks/node_modules_38f2f012._.js"
+],
+    source: "dynamic"
+});
